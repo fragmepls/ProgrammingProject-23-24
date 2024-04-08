114f910e04ae41977ea1cf70a9fd656ee8b736d1
fragmepls <leo.oberrauch@gmail.com>
2024-04-08 16:04:12 +0200

    DBUtils class

---
+++ src/main/java/it/scheduleplanner/dbutils/DBUtils.java
@@ -0,0 +1,59 @@
+package it.scheduleplanner.dbutils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBUtils {
+
+    private static final String URL = "jdbc:sqlite:test.sqlite";
+
+    public static void initializeDatabase() throws SQLException {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS employee ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "name TEXT NOT NULL,"
+                + "age INTEGER)";
+
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute(createTableSQL);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    public static void executeUpdate(String sql) throws SQLException {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        }
+    }
+
+    public static ResultSet executeQuery(String sql) throws SQLException {
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        return statement.executeQuery(sql);
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void closeStatement(Statement statement) throws SQLException {
+        if (statement != null) {
+            statement.close();
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet != null) {
+            resultSet.close();
+        }
+    }
+}
