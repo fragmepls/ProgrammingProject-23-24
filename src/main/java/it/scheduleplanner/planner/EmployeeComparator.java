b77b4fd9205ba2d5a2e18f4ee9e40d049ad0d7ff
Noah.Thanei <NThanei@unibz.it>
2024-05-28 10:51:15 +0200

    Created EmployeeComparator and ScheduleCreator

--- src/main/java/it/scheduleplanner/planner/EmployeeComparator.java
+++ src/main/java/it/scheduleplanner/planner/EmployeeComparator.java
@@ -1,5 +1,6 @@
 package it.scheduleplanner.planner;
 
+import java.time.DayOfWeek;
 import java.util.HashMap;
 
 import it.scheduleplanner.export.Shift;
@@ -7,46 +8,68 @@ import it.scheduleplanner.utils.Employee;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
+
 
 public class EmployeeComparator {
 
-//TODO get employee with least working hours if no employee has enough hours add overtime hours and start again
     public static Map<Employee, Shift> getNext(ArrayList<Employee> employeeList, LocalDate date) {
         Map<Employee, Shift> export = new HashMap<>();
-        for (Employee employee : employeeList) {
-            if (!isAvailable(employee, date)) {
+
+        for (Employee employee1 : employeeList) {
+            if (!isAvailable(employee1, date)) {
                 continue;
             }
-            if (employee.getWorkingHours() - 8 >= 0) {
+            if ((employee1.getWorkingHours()*100) - 8 >= 0) {
                 export.clear();
-                export.put(employee, Shift.FULL);
+                export.put(employee1, Shift.FULL);
                 return export;
             }
-            else if (employee.getWorkingHours() - 4 >= 0) {
+            else if ((employee1.getWorkingHours()*100) - 4 >= 0) {
                 export.clear();
-                export.put(employee, Shift.HALF);
+                export.put(employee1, Shift.HALF);
                 return export;
+            }
+            else {
+                Employee employeeWithMinOvertime = employeeList.get(0);
+                double minOvertimeHours = employeeWithMinOvertime.getOverTimeHours();
+
+                for (Employee employee2 : employeeList) {
+                    double currentOvertimeHours = employee2.getOverTimeHours();
+
+
+                    if (currentOvertimeHours < minOvertimeHours) {
+                        minOvertimeHours = currentOvertimeHours;
+                        employeeWithMinOvertime = employee2;
+
+                    }
+                }
             }
         }
-
 
         return null;
     }
 
     private static boolean isAvailable(Employee employee, LocalDate date) {
-//* TODO if is weekendWorker and date is weekend return true else false
-        //  if employee is on holyday return false
-        if (employee.isWeekendWorker()) {
-
-
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+//        if (!employee.isNotOnVaccation) {
+//            return false;
+//        }
+//        else {
+        if (employee.getFreeDay() == dayOfWeek) {
+            return false;
         }
         else {
-            //skip the weekend
+            if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+                if (!employee.isWeekendWorker()) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return false; //TODO to be changed
     }
-
 }
+
 
 
